ad9d11b6a45c72c86102ed7a3819f162371dc53e
vebjornb <vebjornb@netcompany.com>
2018-12-10 19:10:20 +0100

    fix

--- 2018/01/01.rb
+++ 2018/01/01.rb
@@ -4,10 +4,10 @@ def addNumbersInFile(path)
   arr.each {
     |a| sum+=a.to_i
   }
-  puts sum
+  sum
 end
 
-addNumbersInFile('./2018/01/input.txt')
+p addNumbersInFile('./2018/01/input.txt')
 
 def frequencyReachedTwice(path)
   frequencyFound = false
@@ -22,9 +22,7 @@ def frequencyReachedTwice(path)
     for value in arr
       frequency += value.to_i
       if indexFrequencyMap.key?(frequency)
-        frequencyFound = true
-        puts frequency
-        break
+        return frequency
       else
         indexFrequencyMap[frequency] = index
       end
@@ -34,26 +32,4 @@ def frequencyReachedTwice(path)
   end
 end
 
-frequencyReachedTwice('./2018/01/input.txt')
-
-def frequencyReached(path)
-  frequencyFound = false
-
-  frequencies = [0]
-
-  while frequencyFound == false do
-    arr = IO.readlines(path)
-    for num in arr
-      value = frequencies.last.to_i + num.to_i
-      if frequencies.include?(value)
-        frequencyFound = true
-        break
-      else
-        frequencies.insert(-1, value)
-        puts frequencies
-      end
-    end
-  end
-end
-
-frequencyReached('./2018/01/input.txt')
+p frequencyReachedTwice('./2018/01/input.txt')
