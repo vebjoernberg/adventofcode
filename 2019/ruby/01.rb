a92b7bbdb0403054af19ede0bc5be0f6eaff86fe
Vebjørn Berg <vebjorn.berg@drift.oslo.kommune.no>
2019-12-01 18:40:09 +0100

    Løsning for luke 1

--- 2019/ruby/01.rb
+++ 2019/ruby/01.rb
@@ -1,7 +1,19 @@
 input = File.readlines('/Users/vebjornb/Code/adventofcode/2019/input/input01.txt').map(&:to_i)
 
+# task 1
 result = input.map{ |x| x.div(3) - 2 }.sum
-
 p result
 
+def calculate_fuel(weight)
+  fuel = weight.div(3)-2
+  if fuel > 0
+    fuel + calculate_fuel(fuel)
+  else
+    return 0
+  end
+end
+
+# task 2
+ekstra = input.map{ |x| calculate_fuel(x) }
+p ekstra.sum
 
